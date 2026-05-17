8db88737de72f654b814cf08ff48dee8d38360e8
rasthpop <rasthkp@gmai.com>
2026-05-17 16:56:22 +0300

    fix: moved weather codes to data folder as a separate const

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,6 +5,8 @@ import Button from '../components/button'
 import Slider from '../components/slider'
 import Key from '../components/key'
 
+import { weatherCodes } from '../data/WeatherCodes'
+
 function App() {
   const [worldTab, setWorldTab] = useState(false)
   const [weathTab, setWeathTab] = useState(false)
@@ -67,36 +69,7 @@ function App() {
     const [invalid, setInvalid] = useState(false)
 
     // move to separate file
-    const weatherCodes = {
-      0: "Clear sky",
-      1: "Mainly clear",
-      2: "Partly cloudy",
-      3: "Overcast",
-      45: "Fog",
-      48: "Depositing rime fog",
-      51: "Light drizzle",
-      53: "Moderate drizzle",
-      55: "Dense drizzle",
-      56: "Freezing drizzle",
-      57: "Dense freezing drizzle",
-      61: "Slight rain",
-      63: "Moderate rain",
-      65: "Heavy rain",
-      66: "Freezing rain",
-      67: "Heavy freezing rain",
-      71: "Slight snow fall",
-      73: "Moderate snow fall",
-      75: "Heavy snow fall",
-      77: "Snow grains",
-      80: "Slight rain showers",
-      81: "Moderate rain showers",
-      82: "Violent rain showers",
-      85: "Slight snow showers",
-      86: "Heavy snow showers",
-      95: "Thunderstorm",
-      96: "Thunderstorm with hail",
-      99: "Heavy thunderstorm with hail",
-    }
+
     function clearWeather() {
       setQuery("")
       setInvalid(false)
